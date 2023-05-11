79b726d45617e85e658ae03e4afa126f171caaba
Irina Stepanets <sib408@yandex.ru>
2023-05-11 22:39:09 +0300

    10 sprint

---
+++ src/components/App.js
@@ -0,0 +1,163 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import PopupWithForm from "./PopupWithForm";
+import PopupWithImage from "./PopupWithImage";
+import { api } from "../utils/Api";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+import PopupEditAvatar from "./PopupEditAvatar";
+import PopupEditProfile from "./PopupEditProfile";
+import PopupAddCard from "./PopupAddCard";
+function App() {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [cards, setCards] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    Promise.all([api.getInitialCards(), api.getProfile()])
+      .then(([cards, userData]) => {
+        setCurrentUser(userData);
+        setCards(cards);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    if (!isLiked) {
+      api
+        .addLike(card._id, true)
+        .then((newCard) => {
+          setCards((state) =>
+            state.map((c) => (c._id === newCard._id ? newCard : c))
+          );
+        })
+        .catch((err) => console.log(err));
+    } else {
+      api
+        .deleteLike(card._id)
+        .then((newCard) => {
+          setCards((state) =>
+            state.map((c) => (c._id === newCard._id ? newCard : c))
+          );
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  function handleCardDelete(card) {
+    const isOwn = card.owner._id === currentUser._id;
+    if (isOwn) {
+    api
+      .deleteCard(card._id)
+      .then(() => {
+        setCards(state => state.filter(c => c._id !== card._id));
+      })
+      .catch((err) => console.log(err));
+  }
+}
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setSelectedCard(null);
+  }
+  function handleUpdateUser({ name, about }) {
+    api
+      .editProfile( name, about )
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleUpdateAvatar({ avatar }) {
+    api
+      .changeAvatar( avatar )
+      .then((res) => {
+        setCurrentUser({ ...currentUser, avatar: res.avatar });
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+  function handleAddPlaceSubmit({name, link}) {
+    api
+      .addCard(name, link)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(err));
+  }
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <div className="page">
+          <div className="page__container">
+            <Header />
+            <Main
+              cards={cards}
+              onEditAvatar={handleEditAvatarClick}
+              onEditProfile={handleEditProfileClick}
+              onAddPlace={handleAddPlaceClick}
+              onCardClick={handleCardClick}
+              onCardLike={handleCardLike}
+              onCardDelete={handleCardDelete}
+            />
+            <Footer />
+            <PopupWithForm
+              name="delete"
+              title="Вы уверены?"
+              onClose={closeAllPopups}
+              buttonText={"Да"}
+            ></PopupWithForm>
+
+            <PopupWithImage card={selectedCard} onClose={closeAllPopups} />
+
+            <PopupEditProfile
+              isOpen={isEditProfilePopupOpen}
+              onClose={closeAllPopups}
+              onUpdateUser={handleUpdateUser}
+            />
+
+            <PopupAddCard
+              isOpen={isAddPlacePopupOpen}
+              onClose={closeAllPopups}
+              onAddPlace={handleAddPlaceSubmit}
+            />
+
+            <PopupEditAvatar
+              isOpen={isEditAvatarPopupOpen}
+              onClose={closeAllPopups}
+              onUpdateAvatar={handleUpdateAvatar}
+            />
+          </div>
+        </div>
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
